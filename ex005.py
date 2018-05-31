fc045787499f2db02a04265bfc1fda951b7e5f96
Victor <victor.nicacio@usp.br>
2018-05-30 23:42:26 -0300

    Adição dos exercicios 5 e 6

---
+++ ex005.py
@@ -0,0 +1,2 @@
+num = int(input('Digite um valor: '))
+print('O valor {} tem seu antecessor {} e seu sucessor {}'.format(num, num - 1, num + 1))
---
+++ ex006.py
@@ -0,0 +1,2 @@
+num = int(input('Digite um valor:'))
+print('O valor {}, seu dobro {}, seu triplo {} e a raiz quadrada é {}'.format(num, num*2, num*3, num**(1/2)))
